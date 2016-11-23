320e279952159aa710b89730e3ffbde314287ab3
Alexey <alexei.mgtu@gmail.com>
2016-11-23 20:46:25 +0300

    New class

---
+++ TestClass.java
@@ -0,0 +1,15 @@
+package com.example.alexey.gittest;
+
+/**
+ * Created by Alexey on 23.11.2016.
+ */
+
+public class TestClass {
+
+    private int numberofclasses = 0;
+
+    public TestClass(int num)
+    {
+        this.numberofclasses = num;
+    }
+}
